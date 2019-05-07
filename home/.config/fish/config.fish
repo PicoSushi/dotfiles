1ef8072452ea8fe915773c5d32431ed94d4fc27c
Ryota Kayanuma <kayanuma.ryota+git@donuts.ne.jp>
2019-05-07 16:43:58 +0900

    Delete fish right prompt; breaks everything

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,29 +2,6 @@ function fish_prompt
     ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -shell bare -modules aws,terraform-workspace,venv,user,host,ssh,cwd,perms,jobs,git,hg,exit -newline
 end
 
-function fish_right_prompt
-    set ok_kaomoji "('_')\n(◕‿◕)"
-    set ng_kaomoji "(>_<)\n(;_;)\n(〃＞＿＜;〃)"
-
-    if [ $status -eq 0 ]
-        set_color green
-        printf $ok_kaomoji | sort -R | head -1
-        set_color normal
-    else
-        set_color red
-        printf $ng_kaomoji | sort -R | head -1
-        set_color normal
-    end
-    echo -n "["
-    date "+%Y/%m/%d %H:%M:%S"
-    echo -n "]"
-    if [ -z {$BUNDLE_GEMFILE} ];
-        echo ''
-    else
-        echo '('; and echo {$BUNDLE_GEMFILE} | rev | cut -d '/' -f 2 | rev; and echo ')'
-    end
-end
-
 # pager
 set -gx PAGER less
 if type -q bat
