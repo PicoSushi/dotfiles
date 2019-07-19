e0def2b8dcba2f5e659135d4edb93f83785dd8ed
Ryota Kayanuma <kayanuma.ryota+git@donuts.ne.jp>
2019-07-19 11:17:04 +0900

    Add prompt if powerline not exists

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,8 @@
 function fish_prompt
     if type -q powerline-go
         ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -shell bare -modules aws,terraform-workspace,venv,user,host,ssh,cwd,perms,jobs,git,hg,exit -newline
+    else
+        echo (pwd)\n\>
     end
 end
 
