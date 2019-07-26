f282fb5334c9f2947315e97c750a279694642036
Ryota Kayanuma <kayanuma.ryota+git@donuts.ne.jp>
2019-07-26 18:06:14 +0900

    Improve raw prompt

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,7 +2,7 @@ function fish_prompt
     if type -q powerline-go
         ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -shell bare -modules aws,terraform-workspace,venv,user,host,ssh,cwd,perms,jobs,git,hg,exit -newline
     else
-        echo (pwd)\n\>
+        echo [(whoami)@(hostname)](tput bold)(pwd)(tput sgr0)\n\>
     end
 end
 
