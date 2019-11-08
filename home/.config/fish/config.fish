b6f7719016d5be8038fced9109474b80ceb1cf56
Ryota Kayanuma <r.kayanuma@gigei.jp>
2019-11-08 12:29:59 +0900

    Change PATH loading timing

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,12 @@
+# Golang
+set -x GOPATH $HOME
+set -x PATH $PATH $HOME/bin
+
+# Rust
+if test -d $HOME/.cargo/bin
+    set -x PATH $PATH $HOME/.cargo/bin
+end
+
 function fish_prompt
     if type -q powerline-go
         ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -shell bare -modules aws,terraform-workspace,venv,user,host,ssh,cwd,perms,jobs,git,hg,exit -newline
@@ -37,15 +46,6 @@ end
 # gem bin
 set -gx PATH (gem environment | egrep "EXECUTABLE DIRECTORY" | cut -d ':' -f 2 | cut -d ' ' -f 2) $PATH
 
-# Golang
-set -x GOPATH $HOME
-set -x PATH $PATH $HOME/bin
-
-# Rust
-if test -d $HOME/.cargo/bin
-    set -x PATH $PATH $HOME/.cargo/bin
-end
-
 # fcitx
 if type -q fcitx
     export GTK_IM_MODULE=fcitx
@@ -60,3 +60,6 @@ end
 
 # opam configuration
 source /home/picosushi/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# local config
+export TERM=xterm
