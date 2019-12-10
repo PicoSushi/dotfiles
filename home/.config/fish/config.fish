2feeee7c1b179d5174400d91c8b146be128a1063
Ryota Kayanuma <r.kayanuma@gigei.jp>
2019-12-10 13:47:29 +0900

    Add iTerm integration file loading

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -16,6 +16,11 @@ if test -d $HOME/.cargo/bin
     set -x PATH $PATH $HOME/.cargo/bin
 end
 
+# iTerm2
+if test -f $HOME/.iterm2_shell_integration.(basename $SHELL)
+    source $HOME/.iterm2_shell_integration.(basename $SHELL)
+end
+
 function fish_prompt
     if type -q powerline-go
         ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -shell bare -modules aws,terraform-workspace,venv,user,host,ssh,cwd,perms,jobs,git,hg,exit -newline
