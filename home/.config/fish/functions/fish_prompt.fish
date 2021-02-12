ff4a1118ad4762495b611640bb9ef100e77f32e2
Ryota Kayanuma <r.kayanuma@gigei.jp>
2021-02-12 20:30:17 +0900

    Add jobs for fish

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     if type -q powerline-go
-        ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -shell bare -modules aws,terraform-workspace,venv,user,host,ssh,cwd,perms,jobs,git,hg,exit -newline
+        ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -jobs (jobs -p | wc -l | awk '{print $1}') -modules aws,terraform-workspace,venv,user,host,ssh,jobs,cwd,perms,git,hg,exit -newline
     else
         echo [(whoami)@(hostname)](tput bold)(pwd)(tput sgr0)\n\>
     end
