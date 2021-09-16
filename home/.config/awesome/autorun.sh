bdd7b6082404cb76facbc54fb6a59ffcdfbc747d
Ryota Kayanuma <picosushi12+git@gmail.com>
2021-09-16 13:19:44 +0900

    Use picom

--- home/.config/awesome/autorun.sh
+++ home/.config/awesome/autorun.sh
@@ -2,7 +2,7 @@
 
 # run (only once) processes which spawn with the same name
 function run {
-    if ! pgrep $1 ; then
+    if ! pgrep "$1" ; then
         $@&
     fi
 }
@@ -21,11 +21,14 @@ if ! pgrep xfce4-power-man ; then
     xfce4-power-manager &
 fi
 
-run xfsettingsd
+# run xfsettingsd
 run nm-applet
 run light-locker
-run compton --shadow-exclude '!focused'
-run xcape -e 'Super_L=Super_L|Control_L|Escape'
+# run compton --shadow-exclude '!focused'
+run picom -b
+run pcmanfm --daemon-mode
+
+# run xcape -e 'Super_L=Super_L|Control_L|Escape'
 run thunar --daemon
 run pa-applet
 run pamac-tray
