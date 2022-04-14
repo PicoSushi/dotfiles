677b1d2ca941e81a317586cf1db560d32b646d95
Ryota Kayanuma <picosushi12+git@gmail.com>
2022-04-14 14:35:15 +0900

    Improve

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -8,6 +8,7 @@ function fish_prompt
     set -x POWERLINE_PROMPT_VARIABLE "$fish_prompt_private"
     if type -q powerline-go
         ~/bin/powerline-go -max-width 0 -colorize-hostname -error $status -jobs (jobs -p | wc -l | awk '{print $1}') -shell-var POWERLINE_PROMPT_VARIABLE -shell-var-no-warn-empty -modules aws,terraform-workspace,venv,user,host,shell-var,ssh,jobs,cwd,perms,git,hg,exit -newline
+        set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
     else
         echo [$fish_prompt_private(whoami)@(hostname)](tput bold)(pwd)(tput sgr0)\n\>
     end
