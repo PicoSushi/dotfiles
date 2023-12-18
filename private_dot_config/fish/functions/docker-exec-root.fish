9952d3ecb31c7398f59590e208ae4a0b671fa4fe
Ryota Kayanuma <picosushi12+git@github.com>
2023-12-18 13:26:10 +0900

    Add new function: docker-exec-root

---
+++ private_dot_config/fish/functions/docker-exec-root.fish
@@ -0,0 +1,4 @@
+function docker-exec-root --wraps=docker\ exec\ -it\ -u\ 0\ \(docker\ ps\ \|\ fzf\ --header-lines\ 1\ \|\ awk\ \'\{print\ \$1\}\'\)\ bash --description alias\ docker-exec-root\ docker\ exec\ -it\ -u\ 0\ \(docker\ ps\ \|\ fzf\ --header-lines\ 1\ \|\ awk\ \'\{print\ \$1\}\'\)\ bash
+  docker exec -it -u 0 (docker ps | fzf --header-lines 1 | awk '{print $1}') bash $argv
+        
+end
